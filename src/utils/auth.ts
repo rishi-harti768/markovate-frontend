112824b20f8ddf25fc72083a33a1997d997b734e
rishi-harti768 <rishie.harti@gmail.com>
2024-12-16 10:57:16 +0530

    added auth utilities

---
+++ src/utils/auth.ts
@@ -0,0 +1,75 @@
+const host = process.env.NEXT_PUBLIC_HOST_URL;
+export const authLogin = async (credentials: object) => {
+  try {
+    const response = await fetch(`${host}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(credentials),
+    });
+    const responseText: string = await response.text();
+    const status: number = response.status;
+    return { status, responseText };
+  } catch (error) {
+    return { status: 500 };
+  }
+};
+
+export const authRegister = async (credentials: object) => {
+  try {
+    const response = await fetch(`${host}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(credentials),
+    });
+    const responseText: string = await response.text();
+    const status: number = response.status;
+
+    return { status, responseText };
+  } catch (error) {
+    return { status: 500 };
+  }
+};
+
+export const authforgotpassbefore = async (credentials: object) => {
+  try {
+    const response = await fetch(`${host}/auth/forgot-pass-before`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(credentials),
+    });
+
+    const responseText: string = await response.text();
+    const status: number = response.status;
+
+    return { status, responseText };
+  } catch (error) {
+    return { status: 500 };
+  }
+};
+
+export const authforgotpassafter = async (credentials: object) => {
+  try {
+    const response = await fetch(`${host}/auth/forgot-pass-after`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(credentials),
+    });
+    const responseText: string = await response.text();
+    const status: number = response.status;
+    return { status, responseText };
+  } catch (error) {
+    return { status: 500 };
+  }
+};
