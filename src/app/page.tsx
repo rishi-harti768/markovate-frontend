10e719c27abaed73cbf2cd0afc9f31373d09ae1e
rishi-harti768 <rishie.harti@gmail.com>
2025-01-28 08:22:47 +0530

    home page clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
-"use server";
+"use client";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const Home = () => {
-  /* const host = process.env.NEXT_PUBLIC_HOST_URL;
-  const cookieStore = await cookies();
-  const allCookies = cookieStore.getAll();
-  const cookieHeader = allCookies
-    .map(({ name, value }) => `${name}=${value}`)
-    .join(";");
-  const res = await fetch(`${host}/account/fetch-account`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Cookie: cookieHeader,
-    },
-    credentials: "include",
-  });
-  const responseText: string = await res.text(); */
-
-  // setCookies("test", "test");
-
   return (
     <>
-      <h1>Markovate@official</h1>
-      <p>risji</p>
+      <h1 style={{ marginTop: 200 }}>Markovate@official</h1>
+      <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }}>
+        dialog
+      </motion.button>
     </>
   );
 };
